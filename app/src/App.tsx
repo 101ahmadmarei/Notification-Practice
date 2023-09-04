4f0fc37081b7555c52bf46ede9d1d655575fae31
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2023-09-04 16:39:00 +0300

    fix: :art: notification is success

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,35 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
-import {requestUserPermission} from './utils/pushNotification-helper';
+import React, {useEffect, useState} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {
+  requestUserPermission,
+  NotificationListener,
+} from './utils/pushNotification-helper';
 
 function App() {
   useEffect(() => {
     requestUserPermission();
+    NotificationListener();
   }, []);
+
   return (
-    <View style={{flex: 1, backgroundColor: 'red'}}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.notificationText}>No notification yet</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'purple',
+  },
+  notificationText: {
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
 
 export default App;
--- app/src/utils/pushNotification-helper.js
+++ app/src/utils/pushNotification-helper.js
@@ -1,24 +1,27 @@
 import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert} from 'react-native';
 
 export async function requestUserPermission() {
-  console.log('requestUserPermission');
-  // const authStatus = await messaging().requestPermission();
-  // const enabled =
-  //   authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //   authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  // console.log('requestUserPermission');
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  // if (enabled) {
-  //   console.log('Authorization status:', authStatus);
-  //   GetFCMToken();
-  // }
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    GetFCMToken();
+  }
 }
 
 const GetFCMToken = async () => {
   const fcmToken = await AsyncStorage.getItem('fcmToken');
+  console.log('fcmToken', fcmToken);
   if (!fcmToken) {
     try {
       const token = await messaging().getToken();
+      console.log('token', token);
       if (token) {
         console.log('FCM Token:', token);
         AsyncStorage.setItem('fcmToken', token);
@@ -29,4 +32,28 @@ const GetFCMToken = async () => {
       console.log('error', error);
     }
   }
+};
+
+export const NotificationListener = () => {
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    );
+  });
+
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
+        console.log(
+          'Notification caused app to open from quit state:',
+          remoteMessage.notification,
+        );
+      }
+    });
+
+  messaging().onMessage(async remoteMessage => {
+    console.log('FCM Message Data:', remoteMessage.notification);
+  });
 };
